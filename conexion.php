22ec21197318dead6e2dce37a7124075456be06c
emanuelcarrera <68482024+emanuelcarrera@users.noreply.github.com>
2021-08-16 17:08:14 -0300

    efr

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=practicapro', 'root', '');
-   // echo 'conectado';
-} catch (PDOException $e) {
-    print "¡Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
